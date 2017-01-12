0f561fe9753ad5c89dac04650b457684a4ec6b4a
Vincent Tatan <vincentt.2013@sis.smu.edu.sg>
2017-01-12 12:36:51 +0800

    Restructuring folder, combining kaggle and youtube folder and using it for regression tutorial youtube

--- Investing/nltk/corpus.py
+++
@@ -1,8 +0,0 @@
-from nltk.corpus import gutenberg
-from nltk.tokenize import sent_tokenize
-# print(nltk.__file__)
-
-sample = gutenberg.raw('bible-kjv.txt')
-
-tok = sent_tokenize(sample)
-print(tok[5:15])
--- Machine Learning/buggy.py
+++
@@ -1,7 +0,0 @@
-import pdb
-
-my_list = [4,2,1,True,"abcdefg",3,False,4]
-pdb.set_trace()
-del my_list[3]
-del my_list[3]
-del my_list[4]
